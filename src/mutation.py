598252adf361f8637bfafbc8e6e437a882e479f9
tanbox08 <tanbox@live.com>
2018-11-25 13:39:23 +0800

    Change some words with wrong spelling

--- src/mutation.py
+++ src/mutation.py
@@ -9,9 +9,9 @@ class Mutation(object):
     @staticmethod
     def opMutate(architecture):
         choose_cell = random.randint(0, 1)
-        mutaed_cell = architecture.arch[choose_cell]
+        mutated_cell = architecture.arch[choose_cell]
         choose_node = random.randint(0, 4)
-        mutaed_nodes = mutaed_cell[choose_node]
+        mutated_nodes = mutated_cell[choose_node]
         op = ''
         p = random.random() * 100
         if p <= 5:
@@ -20,15 +20,15 @@ class Mutation(object):
             ran = random.randint(1, 6)
             op = Mutation.OPTYPE[ran]
 
-        mutaed_nodes[random.randint(0, 1)] = op
+        mutated_nodes[random.randint(0, 1)] = op
 
     @staticmethod
     def hidenStateMutate(architecture):
         arch = architecture.arch
         choose_cell = random.randint(0, 1)
-        mutaed_cell = arch[choose_cell]
+        mutated_cell = arch[choose_cell]
         choose_node = random.randint(0, 4)
-        mutated_nodes = mutaed_cell[choose_node]
+        mutated_nodes = mutated_cell[choose_node]
         select_Hiden_num = random.randint(2, 3)
         get_orgin_num = mutated_nodes[select_Hiden_num]
         change_num = random.randint(0, choose_node + 1)
@@ -36,13 +36,13 @@ class Mutation(object):
             change_num = random.randint(0, choose_node + 1)
         mutated_nodes[select_Hiden_num] = change_num
         # change node7
-        if change_num in mutaed_cell[-1]:
-            mutaed_cell[-1].remove(change_num)
+        if change_num in mutated_cell[-1]:
+            mutated_cell[-1].remove(change_num)
         # whether orgin_num in node
         is_point_to_node7 = True
-        for i in mutaed_cell:
+        for i in mutated_cell:
             if get_orgin_num in i:
                 is_point_to_node7 = False
                 break
         if is_point_to_node7:
-            mutaed_cell[-1].append(get_orgin_num)
+            mutated_cell[-1].append(get_orgin_num)
